51bf83b799082b9c2adff1a4459e5c635a1ede0e
Ali Tykhomyrova <43432501+tykhomyrovaAlexandria@users.noreply.github.com>
2020-11-21 20:34:37 -0700

    changes and updates

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
@@ -12,6 +13,7 @@ namespace ContosoUniversity.Models
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
